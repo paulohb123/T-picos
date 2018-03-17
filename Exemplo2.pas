fa241989af81cb9b74199a62814378f766657268
sei la <seila@gmail.com>
2018-03-16 22:12:46 -0300

    Foi pedido para digitar um número

---
+++ Exemplo2.pas
@@ -0,0 +1,9 @@
+Program ola ;
+var n1,dobro:integer;
+Begin
+   writeln ('Digite um número');
+   readln(n1);
+   writeln ('Você digitou o valor ',n1);
+   dobro:=2*n1;
+   writeln('O dobro de ',n1,' é ',dobro);
+End.
